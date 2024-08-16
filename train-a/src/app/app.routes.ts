bcd791b4ee1323405183bd8288860bf6419fe3e2
ARTQ\ChikunovS <ChikunovS@quantumart.ru>
2024-08-16 17:48:39 +0300

    feat: add form

--- train-a/src/app/app.routes.ts
+++ train-a/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainComponent,
     title: 'Main Page',
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    title: 'Sign-Up Page',
   },
 ];
