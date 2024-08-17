7280b1b5f32b9c7d8ae389ba21fd9e374136f335
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-08-18 00:01:53 +0300

    feat: add guard

--- train-a/src/app/app.routes.ts
+++ train-a/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -17,5 +18,6 @@ export const routes: Routes = [
   {
     path: 'signin',
     component: LoginPageComponent,
+    canActivate: [authGuard],
   },
 ];
--- train-a/src/app/services/login.service.ts
+++ train-a/src/app/services/login.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 
@@ -8,12 +9,14 @@ import { catchError, tap } from 'rxjs/operators';
 })
 export class LoginService {
   private readonly http = inject(HttpClient);
+  private readonly router = inject(Router);
   private readonly apiUrl = '/api/signin';
+  private readonly TOKEN_KEY = 'token';
 
   public login(email: string, password: string): Observable<{ token: string }> {
     return this.http.post<{ token: string }>(this.apiUrl, { email, password }).pipe(
       tap((response) => {
-        localStorage.setItem('token', response.token);
+        localStorage.setItem(this.TOKEN_KEY, response.token);
       }),
       catchError(this.handleError),
     );
@@ -33,5 +36,30 @@ export class LoginService {
     }
 
     return throwError(() => new Error(errorMessage));
+  }
+
+  // public userCanActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+  //   console.log('login route', route);
+  //   console.log('login state', state);
+
+  //   if (this.isUserLoggedIn()) {
+  //     return true;
+  //   }
+
+  //   this.router.navigate([this.getLoginUrl()]);
+  //   return false;
+  // }
+
+  // private isUserLoggedIn() {
+  //   // return !!localStorage.getItem(this.TOKEN_KEY);
+  //   return true;
+  // }
+
+  // private getLoginUrl() {
+  //   return '/signin';
+  // }
+
+  public isAuthenticated(): boolean {
+    return !!localStorage.getItem(this.TOKEN_KEY);
   }
 }
